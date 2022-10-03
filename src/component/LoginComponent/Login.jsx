e6cbbaa2ecd0c3402716aec20818e39177886293
Nguyen Tung Anh <nhims0805@gmail.com>
2022-10-03 15:16:11 +0700

    Log in feature
    Login kho vl

--- src/component/LoginComponent/Login.jsx
+++ src/component/LoginComponent/Login.jsx
@@ -1,10 +1,68 @@
+import { Grid, Typography, Box, TextField, Button, Link, Avatar } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
 import React, { useState } from 'react';
+import { findByLabelText } from '@testing-library/react';
 
 const LoginComponent = () => {
+    const paperStyle = {
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
     return (
-        <>
-        Logion
-        </>
+        <Grid container style={paperStyle}>
+            <Box
+                sx={{
+                    width: 300,
+                    height: 350,
+                    padding: 10,
+                    borderRadius: 5,
+                    boxShadow: "0px 6px 6px -3px rgb(0 0 0 / 20%), 0px 10px 14px 1px rgb(0 0 0 / 14%), 0px 4px 18px 3px rgb(0 0 0 / 12%)",
+                }}>
+                <Grid style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main', alignItems: 'center' }}>
+                        <LoginIcon />
+                    </Avatar>
+                </Grid>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Tên đăng nhập"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="password"
+                    label="Mật khẩu"
+                    name="password"
+                    autoComplete="password"
+                    autoFocus
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Đăng nhập
+                </Button>
+                <Grid item xs
+                    sx={{ textAlign: "center" }}>
+                    <Link href="#" variant="body2">
+                        Quên mật khẩu?
+                    </Link>
+                </Grid>
+            </Box >
+        </Grid >
     )
 }
 
