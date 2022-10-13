a0e165cb4e12b3bde6e80ca7d363b6ea3c302b60
Nguyen Tung Anh <nhims0805@gmail.com>
2022-10-13 23:54:16 +0700

    Date picker
    Date pick dang loi

--- src/component/ModalComponent/ModalUpdateAccount.jsx
+++ src/component/ModalComponent/ModalUpdateAccount.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import { Modal } from 'antd';
+import { TextField } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
 
 const ModalUpdateAccount = ({ modalUpdateOpen, setModalUpdateOpen }) => {
     // const [modalUpdateOpen, setModalUpdateOpen] = useState(false);
+    const [value, setValue] = useState < Dayjs | null > (null);
 
     return (
         <>
@@ -13,9 +20,49 @@ const ModalUpdateAccount = ({ modalUpdateOpen, setModalUpdateOpen }) => {
                 onOk={() => setModalUpdateOpen(false)}
                 onCancel={() => setModalUpdateOpen(false)}
             >
-                <p>some contents...</p>
-                <p>some contents...</p>
-                <p>some contents...</p>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Họ và tên"
+                    name="name"
+                    autoComplete="name"
+                    autoFocus
+                // onChange={handleChange}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Tên đăng nhập"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                // onChange={handleChange}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="phonenumber"
+                    label="Số điện thoại"
+                    name="phonenumber"
+                    autoComplete="phonenumber"
+                    autoFocus
+                // onChange={handleChange}
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Basic example"
+                        value={value}
+                        onChange={(newValue) => {
+                            setValue(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
             </Modal>
         </>
     )
