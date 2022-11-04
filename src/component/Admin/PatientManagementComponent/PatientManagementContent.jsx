e58a49ea1b73f936b5d14ebc5e46ae8b1e9053a3
Nguyen Tung Anh <nhims0805@gmail.com>
2022-11-04 22:19:40 +0700

    Patient screen + update chua xong
    ád

--- src/component/Admin/PatientManagementComponent/PatientManagementContent.jsx
+++ src/component/Admin/PatientManagementComponent/PatientManagementContent.jsx
@@ -1,7 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { Pagination, Typography, IconButton } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 const PatientManagementContent = () => {
+    const [modalUpdateOpen, setModalUpdateOpen] = useState(false);
+    const [modalDeleteOpen, setModalDeleteOpen] = useState(false);
+    const [modalAddOpen, setModalAddOpen] = useState(false);
     return (
         <>
-            tuyet
+            <Typography
+                component="h1"
+                variant="h5"
+                color="inherit"
+                noWrap
+            >
+                Quản lý bệnh nhân
+            </Typography>
+            <IconButton aria-label="add" onClick={() => {
+                setModalAddOpen(true)
+            }}>
+                <AddIcon /> Thêm mới
+            </IconButton>
+            <Table size="small" style={{ marginTop: "15px" }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>STT</TableCell>
+                        <TableCell>Họ tên</TableCell>
+                        <TableCell>Tên đăng nhập</TableCell>
+                        <TableCell>Số điện thoại</TableCell>
+                        <TableCell>Ngày sinh</TableCell>
+                        <TableCell>Quyền hạn</TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+
+                    <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell>
+                            <IconButton aria-label="edit" onClick={() => {
+                                setModalUpdateOpen(true)
+                                // dispatch(setUserId(item.userId))
+                            }}>
+                                <EditIcon />
+                            </IconButton>
+                        </TableCell>
+                        <TableCell>
+                            <IconButton aria-label="delete" onClick={() => {
+                                setModalDeleteOpen(true)
+                                // dispatch(setUserId(item.userId))
+                            }}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Pagination
+                    count={totalPages}
+                    defaultPage={1}
+                    onChange={(e, pageNumber) => {
+                        setCurrentPage(pageNumber - 1)
+                    }}
+                />
+            </div> */}
         </>
     )
 }
