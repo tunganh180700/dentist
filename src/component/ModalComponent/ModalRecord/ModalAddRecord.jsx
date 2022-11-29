14b1986a6d45e3b8cd72fde062d66aecc1d09a4c
Nguyen Tung Anh <nhims0805@gmail.com>
2022-11-30 02:15:58 +0700

    Record commit
    a

--- src/component/ModalComponent/ModalRecord/ModalAddRecord.jsx
+++ src/component/ModalComponent/ModalRecord/ModalAddRecord.jsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material"
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Modal } from "antd"
@@ -12,11 +12,70 @@ import axiosInstance from "../../../config/customAxios";
 import { useParams } from "react-router-dom";
 import { listTreatingServiceAPI } from "../../../config/baseAPI";
 import { useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddIcon from '@mui/icons-material/Add';
+import ClearIcon from '@mui/icons-material/Clear';
 
 const ModalAddRecord = ({ modalAddOpen, setModalAddOpen }) => {
     const [value, setValue] = useState(null);
     const { id } = useParams()
     const [listTreatingService, setListTreatingService] = useState([])
+
+    const [open, setOpen] = useState(false);
+    const [disable, setDisable] = useState(true);
+    const [showConfirm, setShowConfirm] = useState(false);
+    const [isEdit, setEdit] = useState(false);
+    const [rows, setRows] = useState([
+        { serviceName: "", price: "", discount: "", status: "" },
+    ]);
+
+    const handleAdd = () => {
+        setRows([
+            ...rows,
+            {
+                serviceName: "", price: "", discount: "", status: ""
+            },
+        ]);
+        setEdit(true);
+    };
+
+    const handleEdit = (e) => {
+        // If edit mode is true setEdit will 
+        // set it to false and vice versa
+        setEdit(!isEdit);
+    };
+
+    const handleSave = () => {
+        setEdit(!isEdit);
+        setRows(rows);
+        console.log("saved : ", rows);
+        setDisable(true);
+        setOpen(true);
+    };
+
+    const handleInputChange = (e, index) => {
+        setDisable(false);
+        const { name, value } = e.target;
+        const list = [...rows];
+        list[index][name] = value;
+        setRows(list);
+    };
+
+    const handleConfirm = () => {
+        setShowConfirm(true);
+    };
+
+    const handleRemoveClick = (i) => {
+        const list = [...rows];
+        list.splice(i, 1);
+        setRows(list);
+        setShowConfirm(false);
+    };
+
+    const handleNo = () => {
+        setShowConfirm(false);
+    };
+
     const getServiceTreating = async (id) => {
         try {
             const res = await axiosInstance.get(listTreatingServiceAPI + id)
@@ -116,7 +175,40 @@ const ModalAddRecord = ({ modalAddOpen, setModalAddOpen }) => {
                         />
                     </div>
                     <div className="table" style={{ marginLeft: "150px" }}>
-                        <button style={{}}>+</button>
+                        {isEdit ? (
+                            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                <Button onClick={handleAdd}>
+                                    <AddIcon onClick={handleAdd} />
+                                    ADD
+                                </Button>
+                                {rows.length !== 0 && (
+                                    <div>
+                                        {disable ? (
+                                            <Button disabled align="right" onClick={handleSave}>
+                                                <AddIcon />
+                                                SAVE
+                                            </Button>
+                                        ) : (
+                                            <Button align="right" onClick={handleSave}>
+                                                <AddIcon />
+                                                SAVE
+                                            </Button>
+                                        )}
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <div>
+                                <Button onClick={handleAdd}>
+                                    <AddIcon onClick={handleAdd} />
+                                    ADD
+                                </Button>
+                                <Button align="right" onClick={handleEdit}>
+                                    <AddIcon />
+                                    EDIT
+                                </Button>
+                            </div>
+                        )}
                         <Table size="small" style={{ marginTop: "15px" }}>
                             <TableHead>
                                 <TableRow>
@@ -132,51 +224,124 @@ const ModalAddRecord = ({ modalAddOpen, setModalAddOpen }) => {
                                     <TableCell>
                                         Trạng thái
                                     </TableCell>
-
+                                    <TableCell>
+                                        Xóa
+                                    </TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {/* {listTreatingService?.map(item => ( */}
                                 {listTreatingService?.map(item => (
                                     <TableRow key={item.serviceId}>
-                                        <TableCell>1</TableCell>
-                                        <TableCell>2</TableCell>
-                                        <TableCell>3</TableCell>
-                                        <TableCell>4</TableCell>
+                                        <TableCell>{item.serviceName}</TableCell>
+                                        <TableCell>{item.price}</TableCell>
+                                        <TableCell>{item.discount}</TableCell>
+                                        <TableCell>{item.status}</TableCell>
+                                        <TableCell></TableCell>
                                     </TableRow>
                                 ))}
-                                <TableRow >
-                                    {/* <TableCell>
-                                            <IconButton aria-label="detail" onClick={() => {
-                                                // setModalDetailOpen(true)
-                                                // dispatch(setUserId(item.patientId))
-                                            }}>
-                                                <RemoveRedEyeIcon />
-                                            </IconButton>
-                                        </TableCell> */}
-
-                                    {/* <TableCell>
-                                            <IconButton aria-label="edit" onClick={() => {
-                                                console.log(el.patientRecordId)
-                                                // setModalUpdateOpen(true)
-                                                // dispatch(setUserId(item.patientId))
-                                            }}>
-                                                <EditIcon />
-                                            </IconButton>
-                                        </TableCell>
-                                        <TableCell>
-                                            <IconButton aria-label="delete" onClick={() => {
-                                                // setModalDeleteOpen(true)
-                                                // dispatch(setUserId(item.patientId))
-                                            }}>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                        </TableCell> */}
-                                </TableRow>
-                                {/* 
-                                ))} */}
+                                {rows.map((row, i) => {
+                                    return (
+                                        <TableRow>
+                                            {isEdit ? (
+                                                <>
+                                                    <TableCell padding="none">
+                                                        <input
+                                                            value={row.serviceName}
+                                                            name="serviceName"
+                                                            onChange={(e) => handleInputChange(e, i)}
+                                                        />
+                                                    </TableCell>
+                                                    <TableCell padding="none">
+                                                        <input
+                                                            value={row.price}
+                                                            name="lastname"
+                                                            onChange={(e) => handleInputChange(e, i)}
+                                                        />
+                                                    </TableCell>
+                                                    <TableCell padding="none">
+                                                        <input
+                                                            value={row.discount}
+                                                            name="lastname"
+                                                            onChange={(e) => handleInputChange(e, i)}
+                                                        />
+                                                    </TableCell>
+                                                    <TableCell padding="none">
+                                                        <input
+                                                            value={row.status}
+                                                            name="lastname"
+                                                            onChange={(e) => handleInputChange(e, i)}
+                                                        />
+                                                    </TableCell>
+                                                    <TableCell></TableCell>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <TableCell component="th" scope="row">
+                                                        {row.serviceName}
+                                                    </TableCell>
+                                                    <TableCell component="th" scope="row">
+                                                        {row.price}
+                                                    </TableCell>
+                                                    <TableCell component="th" scope="row" align="center">
+                                                        {row.discount}
+                                                    </TableCell>
+                                                    <TableCell
+                                                        component="th"
+                                                        scope="row"
+                                                        align="center"
+                                                    >{row.status}</TableCell>
+                                                    <TableCell></TableCell>
+                                                </>
+                                            )}
+                                            {isEdit ? (
+                                                <Button className="mr10" onClick={handleConfirm} >
+                                                    <ClearIcon />
+                                                </Button>
+                                            ) : (
+                                                <div style={{ marginTop: "10px" }}></div>
+                                                // <TableCell></TableCell>
+                                            )}
+                                            {showConfirm && (
+                                                <div>
+                                                    <Dialog
+                                                        open={showConfirm}
+                                                        onClose={handleNo}
+                                                        aria-labelledby="alert-dialog-title"
+                                                        aria-describedby="alert-dialog-description"
+                                                    >
+                                                        <DialogTitle id="alert-dialog-title">
+                                                            {"Confirm Delete"}
+                                                        </DialogTitle>
+                                                        <DialogContent>
+                                                            <DialogContentText id="alert-dialog-description">
+                                                                Are you sure to delete
+                                                            </DialogContentText>
+                                                        </DialogContent>
+                                                        <DialogActions>
+                                                            <Button
+                                                                onClick={() => handleRemoveClick(i)}
+                                                                color="primary"
+                                                                autoFocus
+                                                            >
+                                                                Yes
+                                                            </Button>
+                                                            <Button
+                                                                onClick={handleNo}
+                                                                color="primary"
+                                                                autoFocus
+                                                            >
+                                                                No
+                                                            </Button>
+                                                        </DialogActions>
+                                                    </Dialog>
+                                                </div>
+                                            )}
+                                        </TableRow>
+                                    );
+                                })}
                             </TableBody>
                         </Table>
+
                     </div>
                 </div>
             </Modal>
---
+++ src/redux/RecordSlice/choosenRecord.js
@@ -0,0 +1,15 @@
+const initState = {
+    choosenRecord: {},
+    status: false,
+    statusUser: false,
+    statusDeletePatient: false,
+    patientName: '',
+    birthdate: '',
+    gender: false,
+    address: '',
+    phone: '',
+    email: '',
+    bodyPrehistory: '',
+    teethPrehistory: ''
+
+}
