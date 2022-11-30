1e14d41667a126cb8392b7798ce84c09e78c5794
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-11-30 22:51:01 +0700

    add loaddata, and logic check in, check out

--- src/component/Admin/TimekeepingManagementComponet/TableTimeKeepingManagement.jsx
+++ src/component/Admin/TimekeepingManagementComponet/TableTimeKeepingManagement.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import moment from "moment";
+import { render } from "@testing-library/react";
 
 const TableTimeKeepingManagement = (props) => {
-    const { listTimekeeping } = props
+    const { listTimekeeping } = props;
+    const renderTime = (time) => {
+        if (time === null) return ''
+        return moment(time).format('DD / MM / YYYY, h:mm:ss a')
+    }
+    const renderStatus = (time) => {
+        if (time === null) return "Check in thành công"
+        else return "Check out thành công"
+    }
     return (
         <>
             <Table size="small" style={{ marginTop: "15px" }}>
                 <TableHead>
                     <TableRow>
-                        <TableCell>Ngày</TableCell>
+                        <TableCell>STT</TableCell>
                         <TableCell>Thời gian check-in</TableCell>
                         <TableCell>Thời gian check-out</TableCell>
                         <TableCell>Trạng thái</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {listTimekeeping.map((item) =>
+                    {listTimekeeping.map((item, index) =>
                         <TableRow key={item.timekeepingId}>
-                            <TableCell>{item.timekeepingId}</TableCell>
-                            <TableCell>{item.timekeepingId}</TableCell>
-                            <TableCell>{item.timekeepingId}</TableCell>
-                            <TableCell>{item.timekeepingId}</TableCell>
+                            <TableCell>{index + 1}</TableCell>
+                            <TableCell>{renderTime(item.timeCheckin)}</TableCell>
+                            <TableCell>{renderTime(item.timeCheckout)}</TableCell>
+                            <TableCell>{renderStatus(item.timeCheckout)}</TableCell>
                         </TableRow>
                     )}
                 </TableBody>
--- src/component/Admin/TimekeepingManagementComponet/TimekeepingManagementContent.jsx
+++ src/component/Admin/TimekeepingManagementComponet/TimekeepingManagementContent.jsx
@@ -1,9 +1,42 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { LoadingButton } from '@mui/lab';
+import { Button, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { CHECK_IN, CHECK_OUT, GET_LIST_TIMEKEEPING } from '../../../config/baseAPI';
+import axiosInstance from '../../../config/customAxios';
 import TableTimeKeepingManagement from './TableTimeKeepingManagement';
 
 const TimekeepingManagementContent = () => {
     const [listTimekeeping, setListTimekeeping] = useState([])
+    const role = localStorage.getItem('role');
+    const [loading, setLoading] = useState(false)
+    const [isCheckin, setIsCheckin] = useState(true)
+    const loadData = async () => {
+        try {
+            const res = await axiosInstance.get(GET_LIST_TIMEKEEPING)
+            console.log(res)
+            const { checkinEnable, timekeepingDTOS } = res.data
+            setIsCheckin(checkinEnable)
+            setListTimekeeping(timekeepingDTOS.content)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const checkInOut = async () => {
+        setLoading(true)
+        try {
+            const res = await axiosInstance.post(isCheckin ? CHECK_IN : CHECK_OUT)
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }
+        setLoading(false)
+    }
+    const handleOnClick = () => {
+        checkInOut()
+    }
+    useEffect(() => {
+        loadData()
+    }, [])
     return (
         <>
             <Typography
@@ -14,6 +47,9 @@ const TimekeepingManagementContent = () => {
             >
                 Chấm công
             </Typography>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+                {role !== 'Admin' && <LoadingButton type='primary' onClick={handleOnClick} loading={loading}>Check {isCheckin ? "in" : "out"}</LoadingButton>}
+            </div>
             <TableTimeKeepingManagement listTimekeeping={listTimekeeping} />
         </>
     )
