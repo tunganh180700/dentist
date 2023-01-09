8578e90625a14d50a00fbadaaaa9aa02e0ca1d8c
duc <ducnhse20@gmail.com>
2023-01-09 19:12:07 +0700

    update

--- src/component/Admin/ServiceAndCategoryManagementComponent/ServiceAndCategoryManagementContent.jsx
+++ src/component/Admin/ServiceAndCategoryManagementComponent/ServiceAndCategoryManagementContent.jsx
@@ -2,14 +2,12 @@ import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Pagination, Typography, IconButton } from "@mui/material";
+import { Pagination, Typography, IconButton, Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setCategoryServicedId,
-  setCategoryServiceId,
 } from "../../../redux/modalSlice";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -34,6 +32,12 @@ import ModalDeleteService from "../../ModalComponent/ModalService/ModalDeleteSer
 import ModalAddService from "../../ModalComponent/ModalService/ModalAddService";
 import ModalUpdateService from "../../ModalComponent/ModalService/ModalUpdateService";
 import { setServicedId } from "../../../redux/modalSlice";
+import {
+  StyledTableCell,
+  StyledTableRow,
+  StyledTable,
+} from "../../ui/TableElements";
+import InputDentist from "../../ui/input";
 
 const color = {
   border: "rgba(0, 0, 0, 0.2)",
@@ -68,6 +72,7 @@ const ServiceAndCategoryManagementContent = () => {
   const id = useSelector((state) => state.listCategory.id);
   const [loading, setLoading] = useState();
 
+  const [originData, setOriginData] = useState([]);
   const [categoryServiceIds, setCategoryServiceIds] = useState([]);
   const [categoryServiceId, setCategoryServiceId] = useState();
 
@@ -76,6 +81,8 @@ const ServiceAndCategoryManagementContent = () => {
 
   const [serviceId, setServiceId] = useState();
   const [categoryId, setCategoryId] = useState();
+  const [searchCategory, setSearchCategory] = useState("");
+  const [categorySelected, setCategorySelected] = useState();
 
   console.log("list: ", listCategory);
 
@@ -83,10 +90,9 @@ const ServiceAndCategoryManagementContent = () => {
     setLoading(true);
     try {
       const res = await axiosInstance.get(listAllCategoryAPI);
-      console.log("du lieu check: ", res.data);
       setCategoryServiceId(res.data[0].categoryServiceId);
-      setCategoryServiceIds(res.data);
-      setCategoryId(listCategory[0].categoryServiceId);
+      setOriginData(res.data);
+      setCategoryId(listCategory[0]?.categoryServiceId);
     } catch (error) {
       console.log(error);
     }
@@ -96,6 +102,19 @@ const ServiceAndCategoryManagementContent = () => {
   useEffect(() => {
     loadCategory();
   }, [isAddCategory, isUpdateCategory]);
+
+  useEffect(() => {
+    if (originData.length) {
+      setCategoryServiceIds(
+        originData.filter(
+          (item) =>
+            item?.categoryServiceName
+              .toUpperCase()
+              .indexOf(searchCategory.toUpperCase()) !== -1
+        )
+      );
+    }
+  }, [searchCategory, originData]);
 
   const loadServiceByCategoryId = async (categoryServiceId) => {
     setLoading(true);
@@ -128,190 +147,244 @@ const ServiceAndCategoryManagementContent = () => {
       >
         Bảng Giá Dịch Vụ Nha Khoa
       </Typography>
-      {loading === false && (
-        <>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <Box sx={{ maxWidth: 250, minWidth: 250 }}>
-              <FormControl fullWidth>
-                <InputLabel id="material">Category</InputLabel>
-                <Select
+      {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ maxWidth: 250, minWidth: 250 }}>
+          <FormControl fullWidth>
+            <InputLabel id="material">Category</InputLabel>
+            <Select
+              style={{
+                padding: "25px 0",
+                paddingRight: "10px",
+                width: "100%",
+              }}
+              labelId="material"
+              id="materialSelect"
+              label="Vật liệu"
+              value={categoryServiceId}
+              onChange={(e) => setCategoryServiceId(e.target.value)}
+            >
+              <div>
+                <IconButton
+                  aria-label="add"
                   style={{
-                    padding: "25px 0",
-                    paddingRight: "10px",
+                    borderRadius: "5%",
                     width: "100%",
+                    fontSize: "16px",
                   }}
-                  labelId="material"
-                  id="materialSelect"
-                  label="Vật liệu"
-                  value={categoryServiceId}
-                  onChange={(e) => setCategoryServiceId(e.target.value)}
+                  onClick={() => {
+                    setModalAddCategoryOpen(true);
+                  }}
                 >
-                  <div>
-                    <IconButton
-                      aria-label="add"
-                      style={{
-                        borderRadius: "5%",
-                        width: "100%",
-                        fontSize: "16px",
-                      }}
-                      onClick={() => {
-                        setModalAddCategoryOpen(true);
-                      }}
-                    >
-                      <AddIcon />
-                      Thêm loại dịch vụ
-                    </IconButton>
-                  </div>
-                  {categoryServiceIds?.map((item) => (
-                    <MenuItem
-                      selected={categoryServiceId}
-                      key={item.categoryServiceId}
-                      value={item.categoryServiceId}
-                    >
-                      {item.categoryServiceName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-            <div>
-              <IconButton
-                aria-label="add"
-                style={{
-                  borderRadius: "6px",
-                  border: `1px solid ${color.border}`,
-                  fontSize: "16px",
-                }}
-                onClick={() => {
-                  setModalAddServiceOpen(true);
-                }}
-              >
-                <AddIcon />
-                Thêm mới Dịch vụ
-              </IconButton>
-              <IconButton
-                aria-label="edit-service"
-                style={{
-                  borderRadius: "6px",
-                  border: `1px solid ${color.border}`,
-                  marginRight: "10px",
-                  fontSize: "16px",
-                }}
-                onClick={() => {
-                  setModalUpdateOpen(true);
-                  dispatch(setCategoryServicedId(categoryServiceId));
-                }}
-              >
-                <DesignServicesIcon />
-                Cập nhật loại dịch vụ
-              </IconButton>
-            </div>
-          </div>
-          <div
-            className="table"
+                  <AddIcon />
+                  Thêm loại dịch vụ
+                </IconButton>
+              </div>
+              {categoryServiceIds?.map((item) => (
+                <MenuItem
+                  selected={categoryServiceId}
+                  key={item.categoryServiceId}
+                  value={item.categoryServiceId}
+                >
+                  {item.categoryServiceName}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+        <div>
+          <IconButton
+            aria-label="add"
             style={{
-              marginTop: "15px",
-              textAlign: "center",
-              borderRadius: "8px",
-              padding: "20px",
+              borderRadius: "6px",
               border: `1px solid ${color.border}`,
+              fontSize: "16px",
+            }}
+            onClick={() => {
+              setModalAddServiceOpen(true);
             }}
           >
-            <Table size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell style={{ fontWeight: "bold" }}>Dịch vụ</TableCell>
-                  <TableCell style={{ fontWeight: "bold" }}>Đơn vị</TableCell>
-                  <TableCell style={{ fontWeight: "bold" }}>
-                    Giá trên thị trường
-                  </TableCell>
-                  <TableCell style={{ fontWeight: "bold" }}>
-                    Giá tại nha khoa Nguyễn Trần
-                  </TableCell>
-                  <TableCell />
-                  <TableCell />
-                </TableRow>
-              </TableHead>
-              <TableBody size="medium">
-                {serviceIds?.map((item, index) => (
-                  <TableRow
-                    style={{ textAlign: "center" }}
-                    key={item.serviceId}
-                    size="medium"
-                  >
-                    <TableCell size="medium">
-                      <div style={{width:'300px', overflowX: `${item.serviceName.length > 20 ? 'scroll' : 'unset'} `}}>
-                        <span>{item.serviceName}</span>
-                      </div>
-                    </TableCell>
-                    <TableCell>{item.unit}</TableCell>
-                    <TableCell>{item.marketPrice}</TableCell>
-                    <TableCell>{item.price}</TableCell>
-                    <TableCell>
-                      <IconButton
-                        aria-label="edit"
-                        onClick={() => {
-                          setModalUpdateServiceOpen(true);
-                          dispatch(setServicedId(item.serviceId));
-                        }}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                    </TableCell>
-                    <TableCell>
-                      <IconButton
-                        aria-label="delete"
-                        onClick={() => {
-                          setModalDeleteOpen(true);
-                          dispatch(setServicedId(item.serviceId));
-                        }}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+            <AddIcon />
+            Thêm mới Dịch vụ
+          </IconButton>
+          <IconButton
+            aria-label="edit-service"
+            style={{
+              borderRadius: "6px",
+              border: `1px solid ${color.border}`,
+              marginRight: "10px",
+              fontSize: "16px",
+            }}
+            onClick={() => {
+              setModalUpdateOpen(true);
+              dispatch(setCategoryServicedId(categoryServiceId));
+            }}
+          >
+            <DesignServicesIcon />
+            Cập nhật loại dịch vụ
+          </IconButton>
+        </div>
+      </div> */}
+      <Box className="flex gap-3">
+        <div>
+          <Box className="bg-white p-4 rounded-lg shadow-md mb-3">
+            <InputDentist
+              id="labo"
+              label="Tìm Dịch Vụ"
+              value={searchCategory}
+              onChange={(e) => {
+                setSearchCategory(e.target.value);
+              }}
+            />
+          </Box>
+          <Box className="bg-white py-4 rounded-lg shadow-md overflow-y-scroll max-h-[610px]">
+            <p className="font-bold text-center">
+              Có ( {categoryServiceIds.length} ) kết quả
+            </p>
+            <div className="flex flex-col gap-2 px-3">
+              {categoryServiceIds.map((item, index) => (
+                <Box
+                  className={`whitespace-nowrap p-2 rounded-md cursor-pointer hover:bg-slate-100 ${
+                    categoryServiceId === item.categoryServiceId &&
+                    "bg-sky-100 text-sky-600"
+                  }`}
+                  onClick={() => {
+                    setCategoryServiceId(item.categoryServiceId)
+                  }}
+                >
+                  {item.categoryServiceName}
+                </Box>
+              ))}
+            </div>
+          </Box>
+        </div>
+        <Box className="w-full text-center">
+          <div className="flex gap-3 justify-end mb-3">
+            <Button
+              variant="contained"
+              color="info"
+              onClick={() => {
+                setModalUpdateOpen(true);
+                dispatch(setCategoryServicedId(categoryServiceId));
+              }}
+            >
+              <span className="leading-none">Cập nhật loại dịch vụ</span>
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={() => {
+                setModalAddServiceOpen(true);
+              }}
+            >
+              <span className="leading-none">Thêm mới Dịch vụ</span>
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              endIcon={<DeleteIcon />}
+              onClick={() => {
+                setModalDeleteOpen(true);
+              }}
+            >
+              <span className="leading-none">Xóa labo</span>
+            </Button>
           </div>
+          <StyledTable className="shadow-md text-center" size="small">
+            <TableHead>
+              <StyledTableRow>
+                <StyledTableCell style={{ fontWeight: "bold" }}>
+                  Dịch vụ
+                </StyledTableCell>
+                <StyledTableCell style={{ fontWeight: "bold" }}>
+                  Đơn vị
+                </StyledTableCell>
+                <StyledTableCell style={{ fontWeight: "bold" }}>
+                  Giá trên thị trường
+                </StyledTableCell>
+                <StyledTableCell style={{ fontWeight: "bold" }}>
+                  Giá tại nha khoa Nguyễn Trần
+                </StyledTableCell>
+                <StyledTableCell />
+                <StyledTableCell />
+              </StyledTableRow>
+            </TableHead>
+            <TableBody size="medium">
+              {serviceIds?.map((item, index) => (
+                <StyledTableRow
+                  style={{ textAlign: "center" }}
+                  key={item.serviceId}
+                  size="medium"
+                >
+                  <StyledTableCell size="medium">
+                    {item.serviceName}
+                  </StyledTableCell>
+                  <StyledTableCell>{item.unit}</StyledTableCell>
+                  <StyledTableCell>{item.marketPrice}</StyledTableCell>
+                  <StyledTableCell>{item.price}</StyledTableCell>
+                  <StyledTableCell>
+                    <IconButton
+                      aria-label="edit"
+                      onClick={() => {
+                        setModalUpdateServiceOpen(true);
+                        dispatch(setServicedId(item.serviceId));
+                      }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => {
+                        setModalDeleteOpen(true);
+                        dispatch(setServicedId(item.serviceId));
+                      }}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </StyledTable>
+        </Box>
+      </Box>
 
-          <div>
-            <ModalUpdateCategory
-              modalUpdateOpen={modalUpdateOpen}
-              setModalUpdateOpen={setModalUpdateOpen}
-            />
-          </div>
+      <div>
+        <ModalUpdateCategory
+          modalUpdateOpen={modalUpdateOpen}
+          setModalUpdateOpen={setModalUpdateOpen}
+        />
+      </div>
 
-          <div>
-            <ModalUpdateService
-              modalUpdateOpen={modalUpdateServiceOpen}
-              setModalUpdateOpen={setModalUpdateServiceOpen}
-              isSubmitForm={setIsSubmitFormService}
-            />
-          </div>
-          <div>
-            <ModalDeleteService
-              modalDeleteOpen={modalDeleteOpen}
-              setModalDeleteOpen={setModalDeleteOpen}
-              isSubmitForm={setIsSubmitFormService}
-            />
-          </div>
-          <div>
-            <ModalAddCategory
-              modalAddCategoryOpen={modalAddCategoryOpen}
-              setModalAddCategoryOpen={setModalAddCategoryOpen}
-            />
-          </div>
+      <div>
+        <ModalUpdateService
+          modalUpdateOpen={modalUpdateServiceOpen}
+          setModalUpdateOpen={setModalUpdateServiceOpen}
+          isSubmitForm={setIsSubmitFormService}
+        />
+      </div>
+      <div>
+        <ModalDeleteService
+          modalDeleteOpen={modalDeleteOpen}
+          setModalDeleteOpen={setModalDeleteOpen}
+          isSubmitForm={setIsSubmitFormService}
+        />
+      </div>
+      <div>
+        <ModalAddCategory
+          modalAddCategoryOpen={modalAddCategoryOpen}
+          setModalAddCategoryOpen={setModalAddCategoryOpen}
+        />
+      </div>
 
-          <div>
-            <ModalAddService
-              modalAddOpen={modalAddServiceOpen}
-              setModalAddOpen={setModalAddServiceOpen}
-              isSubmitForm={setIsSubmitFormService}
-            />
-          </div>
-        </>
-      )}
+      <div>
+        <ModalAddService
+          modalAddOpen={modalAddServiceOpen}
+          setModalAddOpen={setModalAddServiceOpen}
+          isSubmitForm={setIsSubmitFormService}
+        />
+      </div>
     </>
   );
 };
--- src/component/ModalComponent/ModalCategory/ModalUpdateCategory.jsx
+++ src/component/ModalComponent/ModalCategory/ModalUpdateCategory.jsx
@@ -1,86 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import { Modal } from 'antd';
-import 'antd/dist/antd.css';
-import { Typography, TextField } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { Modal } from "antd";
+import "antd/dist/antd.css";
+import { Typography, TextField } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import { updateCategory } from '../../../redux/ServiceAndCategorySlice/listCategorySlice';
-import { getCategoryByIdAPI } from '../../../config/baseAPI';
-import axiosInstance from '../../../config/customAxios';
+import { updateCategory } from "../../../redux/ServiceAndCategorySlice/listCategorySlice";
+import { getCategoryByIdAPI } from "../../../config/baseAPI";
+import axiosInstance from "../../../config/customAxios";
+import InputDentist from "../../ui/input";
 
 const ModalUpdateCategory = ({ modalUpdateOpen, setModalUpdateOpen }) => {
-    const dispatch = useDispatch();
-    const categoryServiceId = useSelector(state => state.modal.categoryServiceId);
-    const [loading, setLoading] = useState();
-    const [value, setValue] = useState(null);
+  const dispatch = useDispatch();
+  const categoryServiceId = useSelector(
+    (state) => state.modal.categoryServiceId
+  );
+  const [loading, setLoading] = useState();
+  const [value, setValue] = useState(null);
 
-    const validationSchema = yup.object({
-        categoryServiceName: yup
-            .string('Nhập category')
-            .max(250, 'Category không thể quá 250 ký tự.')
-            .required('Category là bắt buộc.')
-    });
+  const validationSchema = yup.object({
+    categoryServiceName: yup
+      .string("Nhập category")
+      .max(250, "Category không thể quá 250 ký tự.")
+      .required("Category là bắt buộc."),
+  });
 
-    const formik = useFormik({
-        initialValues: {
-
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            dispatch(updateCategory(values))
-            console.log(values.categoryServiceName)
-            setModalUpdateOpen(false)
-
-        }
-    })
-    const fetchCategory = async (categoryServiceId) => {
-        setLoading(true)
-        try {
-            const res = await axiosInstance.get(
-                getCategoryByIdAPI + categoryServiceId,
-            )
-            console.log(res.data)
-            formik.setValues(res.data)
-        } catch (error) {
-            console.log(error)
-        }
-        setLoading(false)
+  const formik = useFormik({
+    initialValues: {},
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      dispatch(updateCategory(values));
+      setModalUpdateOpen(false);
+    },
+  });
+  const fetchCategory = async (categoryServiceId) => {
+    setLoading(true);
+    try {
+      const res = await axiosInstance.get(
+        getCategoryByIdAPI + categoryServiceId
+      );
+      console.log(res.data);
+      formik.setValues(res.data);
+    } catch (error) {
+      console.log(error);
     }
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        if (categoryServiceId > 0)
-            fetchCategory(categoryServiceId)
-    }, [categoryServiceId])
+  useEffect(() => {
+    if (categoryServiceId > 0) fetchCategory(categoryServiceId);
+  }, [categoryServiceId]);
 
-
-    return (
-        <>
-            <Modal
-                title="Cập nhật dịch vụ"
-                open={modalUpdateOpen}
-                onOk={formik.handleSubmit}
-                onCancel={() => setModalUpdateOpen(false)}
-            >
-                {loading === false && <>
-                    <TextField
-                        margin="normal"
-                        // required
-                        fullWidth
-                        id="categoryServiceName"
-                        label="Loại dịch vụ"
-                        name="categoryServiceName"
-                        autoComplete="categoryName"
-                        value={formik.values.categoryServiceName}
-                        autoFocus
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.categoryServiceName && <Typography style={{ color: 'red' }}>{formik.errors.categoryServiceName}</Typography>}
-                </>}
-
-            </Modal>
-        </>
-    )
-}
+  return (
+    <>
+      <Modal
+        title="Cập nhật loại dịch vụ"
+        open={modalUpdateOpen}
+        onOk={formik.handleSubmit}
+        onCancel={() => setModalUpdateOpen(false)}
+      >
+        <InputDentist
+          margin="normal"
+          required
+          id="categoryServiceName"
+          label="Loại dịch vụ"
+          isEdit
+          value={formik.values.categoryServiceName}
+          autoFocus
+          onChange={formik.handleChange}
+          error={{
+            message: formik.errors.categoryServiceName,
+            touched: formik.touched.categoryServiceName,
+          }}
+        />
+      </Modal>
+    </>
+  );
+};
 
 export default ModalUpdateCategory;
